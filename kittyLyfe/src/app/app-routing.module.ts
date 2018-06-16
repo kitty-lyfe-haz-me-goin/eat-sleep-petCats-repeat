28ec6aed4fbe09b891c87256b02f4024cdbec7d5
Ishank Tandon <Ishank.Tandon@homeadvisor.com>
2018-06-16 02:59:24 -0400

    auth stuff. images. create account button. auth service.

--- kittyLyfe/src/app/app-routing.module.ts
+++ kittyLyfe/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent }
+  { path: '', pathMatch: 'full', component: SigninComponent },
+  { path: 'signin', pathMatch: 'full', component: SigninComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- kittyLyfe/src/app/app.module.ts
+++ kittyLyfe/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from '../services/auth.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import {
   MatAutocompleteModule,
@@ -36,6 +38,7 @@ import {
 } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
 
 export const MaterialModules = [
   MatAutocompleteModule,
@@ -72,11 +75,14 @@ export const MaterialModules = [
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule, // imports firebase/storage only needed for storage features
     BrowserAnimationsModule,
+
     MaterialModules,
     FormsModule,
     AppRoutingModule,
+    FlexLayoutModule
   ],
-  declarations: [ AppComponent, HomeComponent ],
+  declarations: [ AppComponent, HomeComponent, SigninComponent ],
+  providers: [AuthService],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
